b88a8d73a63ac7a6dfc4bb8eb3dc6e90a9b1c0cf
Daniel Santiago <danyboricua91@gmail.com>
2024-11-18 13:36:47 -0500

    Initial commit

---
+++ app/src/main/java/dany/nav/MainActivity.kt
@@ -0,0 +1,41 @@
+package dany.nav
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dany.nav.ui.theme.DanyNavDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DanyNavDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainContent(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    Text(text = "Nothing here yet...", modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainPreview() {
+    DanyNavDemoTheme {
+        MainContent()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = uri("https://androidx.dev/snapshots/builds/12671848/artifacts/repository"))
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = uri("https://androidx.dev/snapshots/builds/12671848/artifacts/repository"))
+    }
+}
+
+rootProject.name = "DanyNavDemo"
+include(":app")
+ 
